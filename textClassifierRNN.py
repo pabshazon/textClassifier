3b69730efd560535a77efe819294ddf56dc2086f
Hazon <pabs.ferrari@gmail.com>
2018-04-09 12:54:20 -0700

    Update textClassifierRNN.py

--- textClassifierRNN.py
+++ textClassifierRNN.py
@@ -1,5 +1,5 @@
-# author - Richard Liao
-# Dec 26 2016
+# author - Richard Liao - adapted to python3 by @pablo
+# Dec 26 2016 - Apr 8 2018
 import numpy as np
 import pandas as pd
 import _pickle as cPickle
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 import sys
 import os
 
-os.environ['KERAS_BACKEND']='theano'
+os.environ['KERAS_BACKEND'] = 'theano'
 
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
@@ -31,6 +31,7 @@ MAX_NB_WORDS = 20000
 EMBEDDING_DIM = 100
 VALIDATION_SPLIT = 0.2
 
+
 def clean_str(string):
     """
     Tokenization/string cleaning for dataset
@@ -41,19 +42,19 @@ def clean_str(string):
     string = re.sub(r"\"", "", string)
     return string.strip().lower()
 
-data_train = pd.read_csv('~/devLab/dataset/labeledTrainData.tsv', sep='\t')
+
+data_train = pd.read_csv('labeledTrainData.tsv', sep='\t')
 print(data_train.shape)
 
 texts = []
 labels = []
 
 for idx in range(data_train.review.shape[0]):
-    text = BeautifulSoup(data_train.review[idx], "lxml")
-    texts.append(clean_str(text.get_text().encode('ascii','ignore').decode('utf-8')))
+    text = BeautifulSoup(data_train.review[idx])
+    texts.append(clean_str(text.get_text().encode('ascii', 'ignore').decode('utf-8')))
     labels.append(data_train.sentiment[idx])
 
-
-tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
+tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
 tokenizer.fit_on_texts(texts)
 sequences = tokenizer.texts_to_sequences(texts)
 
@@ -82,7 +83,7 @@ print(y_train.sum(axis=0))
 print(y_val.sum(axis=0))
 
 embeddings_index = {}
-f = open("/home/pablo/devLab/dataset/glove.6B.100d.txt")
+f = open("glove.6B.100d.txt", 'r+', encoding="utf-8")
 for line in f:
     values = line.split()
     word = values[0]
@@ -98,7 +99,7 @@ for word, i in word_index.items():
     if embedding_vector is not None:
         # words not found in embedding index will be all-zeros.
         embedding_matrix[i] = embedding_vector
-        
+
 embedding_layer = Embedding(len(word_index) + 1,
                             EMBEDDING_DIM,
                             weights=[embedding_matrix],
@@ -119,32 +120,34 @@ model.summary()
 model.fit(x_train, y_train, validation_data=(x_val, y_val),
           epochs=10, batch_size=50)
 
-# Attention GRU network		  
+
+# Attention GRU network
 class AttLayer(Layer):
     def __init__(self, **kwargs):
         self.init = initializers.get('normal')
-        #self.input_spec = [InputSpec(ndim=3)]
+        # self.input_spec = [InputSpec(ndim=3)]
         super(AttLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        assert len(input_shape)==3
-        #self.W = self.init((input_shape[-1],1))
+        assert len(input_shape) == 3
+        # self.W = self.init((input_shape[-1],1))
         self.W = self.init((input_shape[-1],))
-        #self.input_spec = [InputSpec(shape=input_shape)]
+        # self.input_spec = [InputSpec(shape=input_shape)]
         self.trainable_weights = [self.W]
         super(AttLayer, self).build(input_shape)  # be sure you call this somewhere!
 
     def call(self, x, mask=None):
         eij = K.tanh(K.dot(x, self.W))
-        
+
         ai = K.exp(eij)
-        weights = ai/K.sum(ai, axis=1).dimshuffle(0,'x')
-        
-        weighted_input = x*weights.dimshuffle(0,1,'x')
+        weights = ai / K.sum(ai, axis=1).dimshuffle(0, 'x')
+
+        weighted_input = x * weights.dimshuffle(0, 1, 'x')
         return weighted_input.sum(axis=1)
 
     def get_output_shape_for(self, input_shape):
         return (input_shape[0], input_shape[-1])
+
 
 embedding_matrix = np.random.random((len(word_index) + 1, EMBEDDING_DIM))
 for word, i in word_index.items():
@@ -152,14 +155,12 @@ for word, i in word_index.items():
     if embedding_vector is not None:
         # words not found in embedding index will be all-zeros.
         embedding_matrix[i] = embedding_vector
-        
+
 embedding_layer = Embedding(len(word_index) + 1,
                             EMBEDDING_DIM,
                             weights=[embedding_matrix],
                             input_length=MAX_SEQUENCE_LENGTH,
                             trainable=True)
-
-
 
 sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
 embedded_sequences = embedding_layer(sequence_input)
